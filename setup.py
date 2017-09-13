a819cdded71077d9b4556dd53d4cd6b06b4a4306
Brandon Wagner <brandon@tripletech.net>
2017-09-13 09:51:40 -0400

    upp both

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ This utility converts Terraform output to json or key-value format to be passed 
 setup(
     name = 'toi',
     packages = ['toi'],
-    version = '1.0.1',
+    version = '1.0.2',
     license='MIT',
     description = 'Converts terraform output to key/vals or json',
     long_description=long_description,
@@ -18,7 +18,7 @@ setup(
     author_email = 'brandon@brandonwagner.info',
     maintainer = 'Brandon Wagner',
     url = 'https://github.com/bwagner5/toi',
-    download_url = 'https://github.com/bwagner5/toi/archive/1.0.0.tar.gz',
+    download_url = 'https://github.com/bwagner5/toi/archive/1.0.2.tar.gz',
     keywords = ['Terraform', 'Packer', 'HashiCorp'],
     zip_safe = True,
     install_requires=[]
